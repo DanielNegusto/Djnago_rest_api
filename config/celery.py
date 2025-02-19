7b8558c2f03b5c1d2532edc154da71514390c821
Даниил Ефремов <de312451@gmail.com>
2025-02-19 03:18:25 +0300

    Docker_34_2

--- config/celery.py
+++ config/celery.py
@@ -3,7 +3,7 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-app = Celery('Djnago_rest_api')
+app = Celery('Django_rest_api')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
